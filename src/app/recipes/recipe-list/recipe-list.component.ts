676ac1e16c17ae699edd8d7908dfe2b72af487d6
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-11-12 20:19:53 +0200

    3-12: added recipe-details view skeleton

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,7 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is just a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A test recipe 2', 'This is just another test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
   ];
 
   constructor() { }
