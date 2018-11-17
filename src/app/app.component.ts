2f6b35d0e5f296bf2579ba26b3646237c633911f
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-11-17 14:32:48 +0200

    6-2: added navigation to the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-app';
+  // title = 'recipe-app';
+  displayedHeader = 'Recipes';
+
+  onHeaderLinkClicked(headerName: string) {
+    this.displayedHeader = headerName;
+
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,8 +10,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is just a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A test recipe 2', 'This is just another test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A test recipe', 'This is just a test', 'https://bit.ly/2qZb3xz'),
+    new Recipe('A test recipe 2', 'This is just another test', 'https://bit.ly/2qZb3xz'),
   ];
 
   constructor() { }
