46ade2fae8a71457a46c5723028cd54a39a32608
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-11-19 17:04:20 +0200

    8-1: Max's solution

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,28 +1,19 @@
 import { Directive,
   ElementRef,
   HostListener,
-  OnInit,
-  Renderer2,
   HostBinding
 } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective implements OnInit {
+export class DropdownDirective {
   menuOpened = false;
-  @HostBinding('class') elClass;
-  originalClass: string;
+  @HostBinding('class.open') isOpen = false;
 
   constructor(private elRef: ElementRef) {}
 
-  ngOnInit() {
-    this.originalClass = this.elRef.nativeElement.className;
-    this.elClass = this.originalClass;
-  }
-
-  @HostListener('click') onclick() {
-    this.menuOpened = !this.menuOpened;
-    this.elClass = this.menuOpened ? this.originalClass + ' open' : this.originalClass;
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
   }
 }
