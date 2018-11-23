2889939bdc519d6ed3d3e2c4e1c74d105765d416
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-11-23 23:59:39 +0200

    10-8: my solution to the challenge

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +12,15 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
+  addIngredientsToSL() {
+    this.recipe.ingredients
+      .forEach((ingredient: Ingredient) => {
+        this.shoppingListService.addIngredient(ingredient);
+    });
+  }
 }
