9bd718ca82f2b05e6674a588074ddf36aab3fb5d
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-11-23 20:50:08 +0200

    10-4

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is just a test', 'https://bit.ly/2qZb3xz'),
     new Recipe('A test recipe 2', 'This is just another test', 'https://bit.ly/2qZb3xz'),
